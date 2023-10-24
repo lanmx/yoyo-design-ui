96e30359a54a4d1035bf58d7565fe261b7771bac
lanmx <1375881038@qq.com>
2023-10-24 12:27:27 +0800

    feat:按钮组件开发

--- src/laMex/filter/index.ts
+++
@@ -1,3 +0,0 @@
-export const filter = function (data: any, value: string | Object) {
-  return data.filter((item:any) => item === value)
-}
--- src/laMex/index.ts
+++
@@ -1,6 +0,0 @@
-import { sort } from "./sort";
-import { filter } from "./filter";
-
-export const laMex =  {
-  sort, filter
-}
